9b22a2f04c703447ce3d0ab8800b728c77706b43
JLKennard <jessicakennard@hotmail.co.uk>
2023-04-20 15:19:26 +0100

    updated script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Write your code here
+const bulbList = document.querySelectorAll(".lightbulb");
+const subtitleOn = document.querySelector(".subtitleOn");
+const subtitleOff = document.querySelector(".subtitleOff");
+let countOn = 0;
+
+function eventHandOn() {
+  countOn++;
+  if (countOn === 1) {
+    subtitleOn.innerHTML = `You've turned the lights on ${countOn} time!`;
+  } else {
+    subtitleOn.innerHTML = `You've turned the lights on ${countOn} times!`;
+  }
+  this.classList.add("active");
+}
+
+function eventHandOff() {
+  this.classList.remove("active");
+}
+
+bulbList.forEach((bulb) => {
+  bulb.addEventListener("mouseover", eventHandOn);
+});
+
+bulbList.forEach((bulb) => {
+  bulb.addEventListener("mouseout", eventHandOff);
+});
